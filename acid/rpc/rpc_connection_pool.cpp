a406a86a8cfc6c6b40e699e46be9dc1e313dbc37
kbjcx <lu.kbjcx@gmail.com>
2023-07-22 09:23:54 +0000

    rpc

---
+++ acid/rpc/rpc_connection_pool.cpp
@@ -0,0 +1,277 @@
+#include "rpc_connection_pool.h"
+
+#include "acid/common/channel.h"
+#include "acid/common/config.h"
+#include "acid/common/mutex.h"
+#include "acid/logger/logger.h"
+#include "acid/rpc/protocol.h"
+#include "acid/rpc/rpc.h"
+#include "acid/rpc/serializer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace acid::rpc {
+
+static auto logger = GET_LOGGER_BY_NAME("system");
+
+static ConfigVar<size_t>::ptr g_channel_capacity = Config::look_up<size_t>(
+    "rpc.connection_pool.channel_capacity", 1024, "rpc connection pool channel capacity");
+static uint64_t s_channel_capacity = 1;
+
+struct _RpcConnectionPoolIniter {
+    _RpcConnectionPoolIniter() {
+        s_channel_capacity = g_channel_capacity->get_value();
+        g_channel_capacity->add_listener([](const size_t& old_val, const size_t& new_val) {
+            LOG_INFO(logger) << "rpc connection pool channel capacity changed from " << old_val
+                             << " to " << new_val;
+            s_channel_capacity = new_val;
+        });
+    }
+};
+
+static _RpcConnectionPoolIniter s_initer;
+
+RpcConnectionPool::RpcConnectionPool(uint64_t timeout_ms)
+    : m_is_close(false), m_timeout_ms(timeout_ms), m_channel(s_channel_capacity) {
+}
+
+RpcConnectionPool::~RpcConnectionPool() {
+    close();
+}
+
+void RpcConnectionPool::close() {
+    LOG_DEBUG(logger) << "RpcConnectionPool::close()";
+
+    if (m_is_close) {
+        return;
+    }
+
+    m_is_heart_close = true;
+    m_is_close = true;
+    m_channel.close();
+    if (m_heart_timer) {
+        m_heart_timer->cancel();
+        m_heart_timer.reset();
+    }
+
+    m_discover_handle.clear();
+
+    IOManager::get_this()->del_event(m_registry->get_socket()->get_socket(),
+                                     IOManager::Event::READ);
+    if (m_registry->is_connected()) {
+        m_registry->close();
+    }
+    m_registry.reset();
+}
+
+bool RpcConnectionPool::connect(Address::ptr address) {
+    Socket::ptr sock = Socket::create_tcp(address);
+    if (!sock) {
+        return false;
+    }
+
+    if (!sock->connect(address, m_timeout_ms)) {
+        LOG_ERROR(logger) << "connect to registry fail";
+        m_registry.reset();
+        return false;
+    }
+
+    m_registry = std::make_shared<RpcSession>(sock);
+
+    LOG_DEBUG(logger) << "connect to registry: " << m_registry->get_socket()->to_string();
+
+    IOManager::get_this()->schedule([this]() { handle_recv(); });
+
+    IOManager::get_this()->schedule([this]() { handle_send(); });
+
+    // 服务中心心跳定时器
+    m_heart_timer = IOManager::get_this()->add_timer(
+        30'000,
+        [this]() {
+            LOG_DEBUG(logger) << "heartbeat";
+            if (m_is_heart_close) {
+                LOG_DEBUG(logger) << "registry closed";
+                // 放弃服务中心
+                m_heart_timer->cancel();
+                m_heart_timer.reset();
+            }
+
+            // 创建心跳包
+            Protocol::ptr protocol = Protocol::create(Protocol::MessageType::HEARTBEAT_PACKET, "");
+            // 向send协程发送消息
+            m_channel << protocol;
+            m_is_heart_close = true;
+        },
+        true);
+    return true;
+}
+
+void RpcConnectionPool::handle_send() {
+    Protocol::ptr request;
+    // 通过 Channel 收集调用请求，如果没有消息时 Channel 内部会挂起该协程等待消息到达
+    // Channel 被关闭时会退出循环
+    while (m_channel >> request) {
+        if (!request) {
+            LOG_WARN(logger) << "RpcConnectionPool::handle_send() fail";
+            continue;
+        }
+        // 发送请求
+        m_registry->send_protocol(request);
+    }
+}
+
+void RpcConnectionPool::handle_recv() {
+    if (!m_registry->is_connected()) {
+        return;
+    }
+
+    while (true) {
+        // 接受响应
+        Protocol::ptr response = m_registry->recv_protocol();
+        if (!response) {
+            LOG_WARN(logger) << "RpcConnectionPool::handle_recv() fail";
+            close();
+            break;
+        }
+
+        m_is_heart_close = false;
+        Protocol::MessageType type = response->get_message_type();
+        // 判断响应类型来进行对应的处理
+        switch (type) {
+            case Protocol::MessageType::HEARTBEAT_PACKET:
+                m_is_heart_close = false;
+                break;
+            case Protocol::MessageType::RPC_SERVICE_DISCOVER_RESPONSE:
+                handle_service_discover(response);
+                break;
+            case Protocol::MessageType::RPC_PUBLISH_REQUEST:
+                handle_publish(response);
+                m_channel << Protocol::create(Protocol::MessageType::RPC_PUBLISH_RESPONSE, "");
+                break;
+            case Protocol::MessageType::RPC_SUBSCRIBE_RESPONSE:
+                break;
+            default:
+                LOG_DEBUG(logger) << "protocol: " << response->to_string();
+                break;
+        }
+    }
+}
+
+void RpcConnectionPool::handle_service_discover(Protocol::ptr response) {
+    Serializer s(response->get_content());
+    std::string service;
+    s >> service;
+    std::map<std::string, Channel<Protocol::ptr>>::iterator it;
+
+    LockGuard lock(m_discover_mutex);
+    // 查找该序列号的 Channel 是否还存在，如果不存在直接返回
+    it = m_discover_handle.find(service);
+    if (it == m_discover_handle.end()) {
+        return;
+    }
+
+    // 通过服务名获取等待该结果的 Channel
+    Channel<Protocol::ptr> channel = it->second;
+    // 对该 Channel 发送调用结果唤醒调用者
+    channel << response;
+}
+
+auto RpcConnectionPool::discover(const std::string& name) -> std::vector<std::string> {
+    if (!m_registry || !m_registry->is_connected()) {
+        return {};
+    }
+
+    // 开启一个channel接收调用结果
+    Channel<Protocol::ptr> recv_channel(1);
+
+    std::map<std::string, Channel<Protocol::ptr>>::iterator it;
+    {
+        LockGuard lock(m_discover_mutex);
+        // 将请求序列号与channel关联
+        it = m_discover_handle.emplace(name, recv_channel).first;
+    }
+
+    // 创建请求协议, 付带请求ID
+    Protocol::ptr request = Protocol::create(Protocol::MessageType::RPC_SERVICE_DISCOVER, name);
+
+    // 向send协程发送消息
+    m_channel << request;
+
+    Protocol::ptr response = nullptr;
+    // 等待 response，Channel内部会挂起协程，如果有消息到达或者被关闭则会被唤醒
+    recv_channel >> response;
+
+    {
+        LockGuard lock(m_discover_mutex);
+        m_discover_handle.erase(it);
+    }
+
+    if (!response) {
+        return {};
+    }
+
+    std::vector<Result<std::string>> res;
+    std::vector<std::string> ret;
+    std::vector<Address::ptr> addrs;
+
+    Serializer s(response->get_content());
+    uint32_t cnt;
+    std::string str;
+
+    s >> str >> cnt;
+    for (uint32_t i = 0; i < cnt; ++i) {
+        Result<std::string> tmp;
+        s >> tmp;
+        res.push_back(tmp);
+    }
+
+    if (res.front().get_code() == RPC_NO_METHOD) {
+        return {};
+    }
+
+    for (size_t i = 0; i < res.size(); ++i) {
+        ret.push_back(res.at(i).get_value());
+    }
+
+    if (!m_sub_handle.contains(RPC_SERVICE_SUBSCRIBE + name)) {
+        // 向注册中心订阅服务的变化消息
+        subscribe(RPC_SERVICE_SUBSCRIBE + name, [name, this](Serializer s) {
+            // false为服务下线, true为新服务节点上线
+            bool is_new_server = false;
+            std::string addr;
+            s >> is_new_server >> addr;
+            LockGuard lock(m_connections_mutex);
+            if (is_new_server) {
+                // 一个新的服务提供者节点加入，将服务地址加入服务列表缓存
+                LOG_DEBUG(logger) << "service [ " << name << " : " << addr << " ] join";
+                m_service_cache[name].push_back(addr);
+            }
+            else {
+                // 已有服务提供者节点下线
+                LOG_DEBUG(logger) << "service [ " << name << " : " << addr << " ] quit";
+                // 清理缓存中断开的连接地址
+                auto its = m_service_cache.find(name);
+                if (its != m_service_cache.end()) {
+                    std::erase(its->second, addr);
+                }
+            }
+        });
+    }
+    return ret;
+}
+
+void RpcConnectionPool::handle_publish(Protocol::ptr protocol) {
+    Serializer s(protocol->get_content());
+    std::string key;
+    s >> key;
+    LockGuard lock(m_sub_mutex);
+    auto it = m_sub_handle.find(key);
+    if (it == m_sub_handle.end()) return;
+    it->second(s);
+}
+
+
+}  // namespace acid::rpc
--- acid/rpc/rpc_connection_pool.h
+++ acid/rpc/rpc_connection_pool.h
@@ -12,6 +12,236 @@
 #ifndef ACID_RPC_CONNECTION_POOL_H
 #define ACID_RPC_CONNECTION_POOL_H
 
+#include "acid/common/co_mutex.h"
+#include "acid/common/traits.h"
+#include "route_strategy.h"
+#include "rpc_client.h"
+#include "rpc_session.h"
+#include "serializer.h"
 
+namespace acid::rpc {
+
+class RpcConnectionPool : public std::enable_shared_from_this<RpcConnectionPool> {
+public:
+    using ptr = std::shared_ptr<RpcConnectionPool>;
+    using MutexType = CoMutex;
+    using LockGuard = MutexType::Lock;
+
+    RpcConnectionPool(uint64_t timeout_ms = -1);
+    ~RpcConnectionPool();
+
+    /**
+     * @brief 连接rpc服务中心
+     *
+     * @param address rpc服务中心地址
+     * @return true
+     * @return false
+     */
+    bool connect(Address::ptr address);
+
+    /**
+     * @brief 异步远程过程调用回调函数
+     *
+     * @tparam Params
+     * @param name 函数名
+     * @param params 可变参
+     */
+    template <class... Params>
+    void callback(const std::string& name, Params&&... params) {
+        static_assert(sizeof...(params), "without a callback function");
+        auto tp = std::make_tuple(params...);
+        constexpr auto size = std::tuple_size<typename std::decay<decltype(tp)>::type>::value;
+        auto cb = std::get<size - 1>(tp);
+        static_assert(function_traits<decltype(cb)> {}.arity == 1, "callback type not support");
+        using res = typename function_traits<decltype(cb)>::template args<0>::type;
+        using ret = typename res::raw_type;
+        static_assert(std::invocable<decltype(cb), Result<ret>>, "callback type not support");
+        RpcConnectionPool::ptr self = shared_from_this();
+        IOManager::get_this()->schedule(
+            [cb = std::move(cb), name = std::move(name), tp = std::move(tp), size, self, this]() {
+                auto proxy = [&cb, &name, &tp, &size, &self,
+                              this ]<std::size_t... Index>(std::index_sequence<Index...>) {
+                    cb(call<ret>(name, std::get<Index>(tp)...));
+                };
+
+                proxy(std::make_index_sequence<size - 1> {});
+            });
+    }
+
+    /**
+     * @brief 异步远程过程调用
+     *
+     * @tparam R 返回值类型
+     * @tparam Params 可变参
+     * @param name 调用函数名
+     * @param params
+     * @return Channel<Result<R>>
+     */
+    template <class R, class... Params>
+    Channel<Result<R>> async_call(const std::string& name, Params&&... params) {
+        Channel<Result<R>> channel(1);
+        RpcConnectionPool::ptr self = shared_from_this();
+        IOManager::get_this()->schedule([channel, name, params..., self, this]() mutable {
+            channel << call<R>(name, params...);
+            self.reset();
+        });
+        return channel;
+    }
+
+    /**
+     * @brief 远程过程调用
+     *
+     * @tparam R
+     * @tparam Params
+     * @param name
+     * @param params
+     * @return Result<R>
+     */
+    template <class R, class... Params>
+    Result<R> call(const std::string& name, Params... params) {
+        LockGuard lock(m_connections_mutex);
+        // 从连接池中取出服务连接
+        auto connection = m_connections.find(name);
+        Result<R> result;
+        if (connection != m_connections.end()) {
+            lock.unlock();
+            result = connection->second->template call<R>(name, params...);
+            if (result.get_code() != RPC_CLOSED) {
+                return result;
+            }
+            lock.lock();
+            // 移除失效连接
+            std::vector<std::string>& addrs = m_service_cache[name];
+            // 将失效的远程连接从addrs中移除
+            std::erase(addrs, connection->second->get_socket()->get_remote_address()->to_string());
+            m_connections.erase(name);
+        }
+
+        std::vector<std::string>& addrs = m_service_cache[name];
+        // 如果服务地址缓存为空则重新向服务中心请求服务发现
+        if (addrs.empty()) {
+            if (!m_registry || !m_registry->is_connected()) {
+                result.set_code(RPC_CLOSED);
+                result.set_message("registry closed");
+                return result;
+            }
+            addrs = discover(name);
+            // 如果没有发现服务返回错误
+            if (addrs.empty()) {
+                result.set_code(RPC_NO_METHOD);
+                result.set_message("no method: " + name);
+                return result;
+            }
+        }
+
+        // 选择客户端负载均衡策略，根据路由策略选择服务地址
+        RouteStrategy<std::string>::ptr strategy =
+            RouteEngine<std::string>::query_strategy(Strategy::RANDOM);
+        if (addrs.size()) {
+            const std::string ip = strategy->select(addrs);
+            Address::ptr address = Address::look_up_any(ip);
+            // 选择的服务地址有效
+            if (address) {
+                RpcClient::ptr client = std::make_shared<RpcClient>();
+                // 成功连接上服务器
+                if (client->connect(address)) {
+                    m_connections.emplace(name, client);
+                    lock.unlock();
+                    return client->template call<R>(name, params...);
+                }
+            }
+        }
+
+        result.set_code(RPC_FAIL);
+        result.set_message("call fail");
+        return result;
+    }
+
+    /**
+     * @brief 订阅消息
+     *
+     * @tparam Func
+     * @param key 订阅的key
+     * @param func 回调函数
+     */
+    template <class Func>
+    void subscribe(const std::string& key, Func func) {
+        {
+            LockGuard lock(m_sub_mutex);
+            auto it = m_sub_handle.find(key);
+            if (it != m_sub_handle.end()) {
+                assert(false);
+                return;
+            }
+
+            m_sub_handle.emplace(key, std::move(func));
+        }
+
+        Serializer s;
+        s << key;
+        s.reset();
+        Protocol::ptr response =
+            Protocol::create(Protocol::MessageType::RPC_SUBSCRIBE_REQUEST, s.to_string(), 0);
+        m_channel << response;
+    }
+
+    void close();
+
+private:
+    /**
+     * @brief 服务发现
+     *
+     * @param name 服务名称
+     * @return std::vector<std::string> 服务地址列表
+     */
+    auto discover(const std::string& name) -> std::vector<std::string>;
+
+    /**
+     * @brief rpc 连接对象的发送协程，通过 Channel 收集调用请求，并转发请求给注册中心。
+     */
+    void handle_send();
+
+    /**
+     * @brief rpc 连接对象的接收协程，负责接收注册中心发送的 response 响应并根据响应类型进行处理
+     */
+    void handle_recv();
+
+    /**
+     * @brief 处理注册中心服务发现响应
+     */
+    void handle_service_discover(Protocol::ptr response);
+
+    /**
+     * @brief 处理发布消息
+     */
+    void handle_publish(Protocol::ptr protocol);
+
+private:
+    bool m_is_close = true;
+    bool m_is_heart_close = true;
+    uint64_t m_timeout_ms;
+    // 保护m_connections
+    MutexType m_connections_mutex;
+    // 服务名到全部缓存的服务地址列表映射
+    std::map<std::string, std::vector<std::string>> m_service_cache;
+    // 服务名和服务地址的连接池
+    std::map<std::string, RpcClient::ptr> m_connections;
+    // 服务中心连接
+    RpcSession::ptr m_registry;
+    // 服务中心心跳定时器
+    Timer::ptr m_heart_timer;
+    // 注册中心消息发送通道
+    Channel<Protocol::ptr> m_channel;
+    // 服务名到对应调用者协程的 Channel 映射
+    std::map<std::string, Channel<Protocol::ptr>> m_discover_handle;
+    // m_discover_handle 的 mutex
+    MutexType m_discover_mutex;
+    // 处理订阅的消息回调函数
+    std::map<std::string, std::function<void(Serializer)>> m_sub_handle;
+    // 保护m_subHandle
+    MutexType m_sub_mutex;
+};
+
+}  // namespace acid::rpc
 
 #endif
