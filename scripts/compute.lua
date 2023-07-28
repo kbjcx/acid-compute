8a3bd415d6d8a4a0d62adf40b7063a487a064cfa
llz <lulu5v@163.com>
2023-07-28 08:46:11 +0800

    lua

--- scripts/compute.lua
+++ scripts/compute.lua
@@ -1,5 +1,10 @@
 package.cpath = "/home/llz/CPP/acid-compute/lib/?.so;/home/llz/CPP/acid-compute/modules/?.so"
 local libcompute = require("libcompute")
 
-local result = libcompute.compute(5, 4)
-print("result: " .. result)
+local methods = {}
+
+methods["compute"] = libcompute
+
+function Main(name)
+    return methods[name].compute(a, b)
+end
